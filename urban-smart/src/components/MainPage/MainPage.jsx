86fd8f71b2bb2a6b0f42b80229eb938b2673e1b6
Bekzhan Zhamantayev <bekzzhan31@gmail.com>
2021-10-27 21:19:55 +0600

    init: implement app structure

---
+++ urban-smart/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+export function MainPage () {
+
+  return (
+    <div className="main">
+      <div className="form-wrap">
+        <form className="form">
+          <input type="text" className="input-login" />
+          <input type="password" className="input password" />
+          <button type="submit" className="submit-btn"></button>
+        </form>
+      </div>
+    </div>
+  )
+}
